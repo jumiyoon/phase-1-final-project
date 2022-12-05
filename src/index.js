3c3d3a1873d62e6c4917645342b4ba5a4eb2b199
Jumi Yoon <jums.code@gmail.com>
2022-12-04 20:26:50 -0800

    change innerHTML to appending elements individually; add plus / minus button

--- src/index.js
+++ src/index.js
@@ -1,23 +1,49 @@
 
 // 1. build & add restaurant cards
 function buildCards(eat) {
-    let card = document.createElement("li");
-    card.className = "card";
+    const div = document.createElement("div")
+    div.className = "card"
 
-    // build restaurant card
-    card.innerHTML = `
-    <div class="card">
-    <h4>${eat.name}</h4>
-    <a href=${eat.yelp}><img src=${eat.image} class="eats-img" id=${eat.id}></a>
-    <p><b>Location:</b> ${eat.location}</p>
-    <p><b>Cuisine:</b> ${eat.type}</p>
-    <p><b>Popular Dish:</b> ${eat.popular_dish}</p>
-    <button class="like-button" id=${eat.id}> Likes</button>
-    <div>`
+    const eatsName = document.createElement("h4")
+    eatsName.textContent = `${eat.name}`
 
-    // add card to the DOM
-    document.getElementById("eats-cards").appendChild(card);
+    const eatsYelp = document.createElement("a")
+    eatsYelp.href = `${eat.yelp}`
+
+    const eatsImg = document.createElement("img")
+    eatsImg.className = "eats-img"
+    eatsImg.src = `${eat.image}`
+    
+
+    eatsYelp.append(eatsImg)
+
+    const eatsInfo = document.createElement("p")
+    eatsInfo.innerHTML = `
+    <b>Location:</b> ${eat.location} <br>
+    <b>Cuisine:</b> ${eat.type} <br>
+    <b>Popular Dish:</b> ${eat.popular_dish} <br>`
+
+    const eatsLike = document.createElement("button")
+    eatsLike.className = "like-button"
+    eatsLike.textContent = `${eat.likes} Likes`
+
+    const plus = document.createElement("button")
+    plus.className = "plus"
+    plus.textContent = "+"
+
+    const minus = document.createElement("button")
+    minus.className = "minus"
+    minus.textContent = "-"
+
+    
+
+    div.append(eatsName, eatsYelp, eatsInfo, plus, eatsLike, minus)
+
+    const eatCards = document.querySelector("#eat-cards")
+    document.getElementById("eats-cards").appendChild(div);
+
 }
+
 
 function getCards() {
      // fetch data from JSON server
@@ -28,30 +54,26 @@ function getCards() {
 
 getCards();
 
-
-
-
 // 2. submit new restaurant info
 
-// const formSubmit = document.querySelector(".add-eats")
-// formSubmit.addEventListener("submit", handleSubmit)
+const formSubmit = document.querySelector(".add-eats")
+formSubmit.addEventListener("submit", handleSubmit)
 
-function handleSubmit() {
-    const formSubmit = document.querySelector(".add-eats")
-    formSubmit.addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log(e)
-        let newObj = {
-                "name": e.target[0].value,
-                "location": e.target[1].value,
-                "yelp": e.target[2].value,
-                "image": e.target[3].value,
-                "type": e.target[4].value,
-                "popular_dish": e.target[5].value
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e)
+    let newObj = {
+            "name": e.target[0].value,
+            "location": e.target[1].value,
+            "yelp": e.target[2].value,
+            "image": e.target[3].value,
+            "type": e.target[4].value,
+            "popular_dish": e.target[5].value,
+            "likes": 0
         }
     submitInfo(newObj);
-    })
 }
+
 
 
 function submitInfo(newObj) {
@@ -67,7 +89,14 @@ function submitInfo(newObj) {
     .then (eats => console.log(eats))
 }
 
-handleSubmit();
 
-// 3. likes
-let likes = 0
+
+// 3. hover over image event
+// const eatsImg = document.querySelector(".eats-img")
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     eatsImg.addEventListener("click", () => console.log("I was clicked"))
+
+// })
+
+// document.getElementsByClassName(".eats-cards").querySelectorAll("div.card img")
